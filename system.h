a6d590fd3570e10417559eca5e5799aac6f1b6b8
V1489Cygni <wb0011@mail.ru>
2013-11-03 11:11:24 +0000

    Code formated

--- system.h
+++ system.h
@@ -1,11 +1,6 @@
-//////////////////////////////////////////////////////////////////////
-// File: system.h
-
 #ifndef __SYSTEM_H
 #define __SYSTEM_H
 
-/* MAIN.C */
-//extern void cmain (unsigned long magic, unsigned long addr);
 extern unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count);
 extern unsigned char *memset(unsigned char *dest, unsigned char val, int count);
 extern unsigned short *memsetw(unsigned short *dest, unsigned short val, int count);
@@ -13,7 +8,6 @@ extern int strlen(const char *str);
 extern unsigned char inportb (unsigned short _port);
 extern void outportb (unsigned short _port, unsigned char _data);
 
-/* SCRN.C */
 extern  void cls();
 extern void putch(unsigned char c);
 extern void puts(unsigned char *str);
